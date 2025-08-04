52ff963705264f54d9b1fa3465c42112fb3ed465
nek1t-v1zn1uk <1nik.computer1@gmail.com>
2025-08-04 17:03:07 +0300

    Initial commit

---
+++ src/main/kotlin/com/example/teamroomback/TeamRoomBackApplication.kt
@@ -0,0 +1,11 @@
+package com.example.teamroomback
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TeamRoomBackApplication
+
+fun main(args: Array<String>) {
+    runApplication<TeamRoomBackApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/teamroomback/TeamRoomBackApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.teamroomback
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TeamRoomBackApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

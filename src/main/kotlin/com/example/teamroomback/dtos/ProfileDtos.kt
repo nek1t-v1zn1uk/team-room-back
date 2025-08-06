05f76a1cb257feba079e73181d79b08c27821640
nek1t-v1zn1uk <1nik.computer1@gmail.com>
2025-08-06 21:08:19 +0300

    Creating and getting user`s profile

---
+++ src/main/kotlin/com/example/teamroomback/dtos/ProfileDtos.kt
@@ -0,0 +1,45 @@
+package com.example.teamroomback.dtos
+
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+
+data class CreateProfileRequest(
+    @field:NotBlank(message = "First name cannot be empty")
+    @field:Size(max = 32, message = "First name must be up to 32 symbols")
+    val firstName: String,
+
+    @field:Size(max = 32, message = "Last name must be up to 32 symbols")
+    val lastName: String? = null,
+
+    @field:Size(max = 100, message = "Biography must be up to 100 symbols")
+    val biography: String? = null,
+
+    val photoUrl: String? = null
+)
+data class CreateProfileResponse(
+    val message: String,
+)
+
+data class GetProfileResponse(
+    val firstName: String,
+    val lastName: String? = null,
+    val biography: String? = null,
+    val photoUrl: String? = null
+)
+
+data class UpdateProfileRequest(
+    @field:NotBlank(message = "First name cannot be empty")
+    @field:Size(max = 32, message = "First name must be up to 32 symbols")
+    val firstName: String,
+
+    @field:Size(max = 32, message = "Last name must be up to 32 symbols")
+    val lastName: String? = null,
+
+    @field:Size(max = 100, message = "Biography must be up to 100 symbols")
+    val biography: String? = null,
+
+    val photoUrl: String? = null
+)
+data class UpdateProfileResponse(
+    val message: String,
+)

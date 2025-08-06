c24729e3df5508e5d6703a7a2c1929b8e7e50c13
nek1t-v1zn1uk <1nik.computer1@gmail.com>
2025-08-06 16:13:32 +0300

    Profile entity and DB migration

---
+++ src/main/kotlin/com/example/teamroomback/entities/Profile.kt
@@ -0,0 +1,36 @@
+package com.example.teamroomback.entities
+
+import jakarta.persistence.CascadeType
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.OneToOne
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "profiles")
+data class Profile(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    @Column(name = "first_name", length = 32, nullable = false)
+    val firstName: String,
+
+    @Column(name = "last_name", length = 32)
+    val lastName: String? = null,
+
+    @Column(name = "biography", length = 100)
+    val biography: String? = null,
+
+    @Column(name = "photo_url")
+    val photoUrl: String? = null,
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    val user: User,
+)

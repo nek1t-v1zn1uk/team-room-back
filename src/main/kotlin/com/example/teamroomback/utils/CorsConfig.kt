902851017fa9b7bc440a90a920c6065beb7c6015
nek1t-v1zn1uk <1nik.computer1@gmail.com>
2025-08-08 11:46:22 +0300

    Added CORS configuration for development

---
+++ src/main/kotlin/com/example/teamroomback/utils/CorsConfig.kt
@@ -0,0 +1,23 @@
+package com.example.teamroomback.utils
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfig {
+
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**") // Apply to all endpoints
+                    .allowedOrigins("*") // Replace with your frontend's origin
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(false)
+            }
+        }
+    }
+}
